bc799715c25b21e7c8db28d2afacf7b1952d88b8
DEKVIW <hxw1157816198@163.com>
2024-10-08 09:28:46 +0000

    github action update UTC 2024-10-08 Tuesday 09:28:46 Updated By Github Actions

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "012",
+    "background": "5",
+    "tmpColor": "000000",
+    "tmpSize": "18",
+    "cityColor": "000000",
+    "citySize": "18",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "b003bb51706c4a69994939920191ca30"
+  }
+}
